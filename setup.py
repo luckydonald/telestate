9b662e5ff65e49175e17b5ea55cb22eb1f07f806
luckydonald <mac2019.git@luckydonald.de>
2020-12-18 00:56:29 +0100

    [telestate] Try `find_packages()` to crawl the package names.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 from os import path
 __author__ = 'luckydonald'
+print('Will provide the following packages: ' + str(find_packages()))
 
 here = path.abspath(path.dirname(__file__))
 
@@ -48,7 +49,7 @@ setup(
     keywords='telestate telegram state machine state management statemachine teleflask flask pytgbot telegram bot api',
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['telestate'],
+    packages=find_packages(),
     # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     # List run-time dependencies here. These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
