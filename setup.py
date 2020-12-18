b5449a4b2522806ecf42f57dcf98d322f8516dbd
luckydonald <mac2019.git@luckydonald.de>
2020-12-18 10:05:59 +0100

    Revert: [telestate] Try `find_packages()` to crawl the package names. (bb9cecd0) - Instead try hardcoding them.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     keywords='telestate telegram state machine state management statemachine teleflask flask pytgbot telegram bot api',
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(),
+    packages=['telestate', 'telestate.contrib'],
     # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     # List run-time dependencies here. These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
